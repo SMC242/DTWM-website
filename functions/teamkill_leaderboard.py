7d20c16e112f1650a7380392ad5a8ac709d3768f
SMC242 <56912943+SMC242@users.noreply.github.com>
2021-03-18 23:04:15 +0000

    Using Auraxium instead

--- functions/teamkill_leaderboard.py
+++ functions/teamkill_leaderboard.py
@@ -1,6 +1,7 @@
-import requests as r
-from json import dumps, loads
+import asyncio
+import auraxium
 from typing import List
+from auraxium import ps2
 
 
 def pipe(*funcs):
@@ -12,36 +13,45 @@ def pipe(*funcs):
     return inner
 
 
-def get_data(url): return r.get(url)
-def to_indexable(data): return loads(data.content)
-def prettify(data): return dumps(data, indent=4, sort_keys=True)
-def pretty_print(data): return print(prettify(data))
+def get_dtwm(client: auraxium.Client):
+    DTWM_ID = 37566723466738093
+
+    async def get_dtwm_inner():
+        result = await client.get_by_id(ps2.Outfit, DTWM_ID)
+        if not result:
+            raise ValueError(
+                "Failed to get DTWM outfit. Check that the DTWM id is correct")
+        return result
+    return get_dtwm_inner
 
 
-get_dict = pipe(get_data, to_indexable)
+async def get_members(
+    outfit: ps2.Outfit): return await outfit.members().flatten()
 
 
-def get_keys(keys: List[str]):
-    def get_keys_inner(data: dict):
-        key, *extra = keys
-        print(key)
-        value = data[key]
-        if extra:
-            return get_keys(extra)(value)
-        return value
-    return get_keys_inner
+async def to_character(
+    member: ps2.OutfitMember): return await member.character()
 
 
-def get_keys_from_url(url: str):
-    def get_keys_from_url_inner(keys: List[str]):
-        return pipe(get_dict, get_keys(keys))(url)
-    return get_keys_from_url_inner
+def get_characters(members: List[ps2.OutfitMember]):
+    return asyncio.gather(*[to_character(m) for m in members])
 
 
-def get_members():
-    url = ("https://census.daybreakgames.com/get/ps2:v2/outfit/"
-           + "?alias=DTWM&c:resolve=member_character(name,type.faction)")
-    return get_keys_from_url(url)(["outfit_list", 0, "members"])
+async def get_kills(char: ps2.Character):
+    return await char.events(type="KILL")
 
 
-pretty_print(get_members())
+def kills_per_char(chars: List[ps2.Character]):
+    return asyncio.gather(*[get_kills(c) for c in chars])
+
+
+async def main():
+    async with auraxium.Client() as client:
+        dtwm = await get_dtwm(client)()
+        members = await get_members(dtwm)
+        characters = await get_characters(members)
+        kills_per_member = await kills_per_char(characters)
+        print(kills_per_member)
+
+
+asyncio.get_event_loop().run_until_complete(main())
