5d029bb80dc311f2607fbfcc9514b21a4aac291a
SMC242 <56912943+SMC242@users.noreply.github.com>
2021-03-18 01:57:26 +0000

    WIP // optional page icon prop

--- src/components/templates/page_template.tsx
+++ src/components/templates/page_template.tsx
@@ -1,18 +1,32 @@
 import React, { FC, ReactNode } from "react";
 import Helmet from "react-helmet";
+import Skull from "../../images/skull/skull32.png";
+
+const get_link = (icon: HTMLLinkElement | undefined) =>
+  icon || <link rel="shortcut icon" type="image/x-icon" href={Skull} />;
 
 export interface PageTemplateProps {
   title?: string;
   head?: ReactNode;
   body: ReactNode;
+  /**
+   * Defaults to the DTWM skull
+   */
+  icon?: HTMLLinkElement;
 }
 
-export const PageTemplate: FC<PageTemplateProps> = ({ title, head, body }) => {
+export const PageTemplate: FC<PageTemplateProps> = ({
+  title,
+  head,
+  body,
+  icon,
+}) => {
   return (
     <>
       <Helmet>
         <title>{title}</title>
         <head>{head}</head>
+        {get_link(icon)}
       </Helmet>
       <>{body}</>
     </>
