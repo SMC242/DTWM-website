cc5947e5197e0f2d92cd5361d5007f81a6728e8c
SMC242 <56912943+SMC242@users.noreply.github.com>
2021-03-21 18:40:49 +0000

    Fixed coroutine error
    I need to revise my design as it's getting rate-limited. I think I'll fork off for each member and do all the queries for them before going to the next member. That might reduce the number of queries per second

--- functions/teamkill_leaderboard.py
+++ functions/teamkill_leaderboard.py
@@ -2,6 +2,7 @@ import asyncio
 import auraxium
 from typing import Coroutine, List, Union, Callable
 from auraxium import ps2
+from auraxium.errors import MissingServiceIDError
 from inspect import iscoroutinefunction as is_coro
 
 # constants
@@ -12,7 +13,7 @@ def read_file(path: str):
         return [line.strip() for line in f.readlines()]
 
 
-API_KEY = read_file("PS2_API_KEY.txt")
+API_KEY = read_file("PS2_API_KEY.txt")[0]
 DTWM_ID = 37566723466738093
 
 
@@ -22,13 +23,28 @@ def pipe_async(*funcs: List[Union[Coroutine, Callable]]):
             if is_coro(func):
                 return await func(*args)
             return func(*args)
+        return call_func_inner
 
     async def pipe_async_inner(*args):
         result = await call_func(funcs[0])(args)
-        for func in funcs:
+        for func in funcs[1:]:
             result = await call_func(func)([result])
         return result
     return pipe_async_inner
+
+
+def with_retry(func):
+    async def with_retry_inner(*args):
+        try:
+            result = await func(*args)
+        except MissingServiceIDError:
+            print("Rate limited. Retrying...")
+            for i in range(6):
+                await asyncio.sleep(10)
+                print(f"Waited {i + 1 * 10} seconds so far")
+            result = await func(*args)
+        return result
+    return with_retry_inner
 
 
 def get_dtwm(dtwm_id: int) -> Coroutine[None, None, ps2.Outfit]:
@@ -41,12 +57,12 @@ def get_dtwm(dtwm_id: int) -> Coroutine[None, None, ps2.Outfit]:
     return get_dtwm_inner
 
 
-async def get_members(
-    outfit: ps2.Outfit): return await outfit.members().flatten()
+async def get_members(outfit: ps2.Outfit):
+    return await outfit.members().flatten()
 
 
-async def to_character(
-    member: ps2.OutfitMember): return await member.character()
+async def to_character(member: ps2.OutfitMember):
+    return await member.character()
 
 
 def get_characters(members: List[ps2.OutfitMember]):
@@ -62,12 +78,17 @@ def kills_per_char(chars: List[ps2.Character]):
     return asyncio.gather(*[get_kills(c) for c in chars])
 
 
-get_all_chars = pipe_async(get_dtwm(DTWM_ID), get_members, get_characters)
+get_all_chars = pipe_async(
+    get_dtwm(DTWM_ID),
+    with_retry(get_members),
+    with_retry(get_characters)
+)
 
 
 async def main():
-    async with auraxium.Client() as client:
-        characters = get_all_chars(client)
+    async with auraxium.Client(service_id=API_KEY) as client:
+        characters = await get_all_chars(client)
+
         kills_per_character = kills_per_char(characters)
         print(kills_per_character)
 
