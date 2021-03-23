7c4bfbf73bc82538a073ea5f1e8596bf26b69ea6
SMC242 <56912943+SMC242@users.noreply.github.com>
2021-03-23 14:09:35 +0000

    Still trying to avoid the rate limit

--- functions/teamkill_leaderboard.py
+++ functions/teamkill_leaderboard.py
@@ -77,27 +77,27 @@ async def flatten(gen: AsyncGenerator) -> List[Any]:
     return [e async for e in gen]
 
 
-def limited_flatten(gen: AsyncGenerator) -> List[Any]:
-    async def limited_flatten_inner(limit: int):
-        stop_signal = "STOP_NOW"
-
-        async def inner_iter(index: int) -> Union[Any, str]:
-            if index == limit:
-                return stop_signal
-            return await next(gen)
-
-        results = []
-        for i in range(limit):
-            res = await inner_iter(i)
-            if res == stop_signal:
-                return results
-            results.append(res)
-        return results
+def limited_flatten(gen:  AsyncGenerator[None, Any]):
+    async def limited_flatten_inner(limit: int) -> AsyncGenerator[None, Any]:
+        return [await gen.__anext__() for i in range(limit)]
     return limited_flatten_inner
 
 
+def query_factory(collection: str):
+    def query_factory_inner(**kwargs):
+        def query_factory_inner_2(joins: List[str] = None):
+            joins = joins or []
+            query = Query(collection, service_id=API_KEY, **kwargs)
+            query.limit(1000)
+            for join_collection in joins:
+                query.create_join(join_collection)
+            return query
+        return query_factory_inner_2
+    return query_factory_inner
+
+
 def query_outfit(outfit_id: int):
-    return Query("outfit_member", outfit_id=outfit_id).limit(1000)
+    return query_factory("outfit_member")(outfit_id=outfit_id)()
 
 
 def with_character_query(query: Query):
@@ -118,9 +118,8 @@ def get_chars(outfit_id: int):
 
 
 def total_kills_query(char: ps2.Character):
-    query = Query("event",
-                  type="KILL").limit(1000)
-    query.create_join(f"attacker_character_id={char['character_id']}")
+    query = query_factory("event")(type="KILL")(
+        f"attacker_character_id={char['character_id']}")
     return query
 
 
@@ -132,7 +131,8 @@ def do_total_kills_query(client):
 
 def get_single_char(client: auraxium.Client):
     async def get_single_char_inner(char):
-        q = Query("single_character_by_id", character_id=char["character_id"])
+        q = query_factory("single_character_by_id")(
+            character_id=char["character_id"])()
         return await client.request(q)
     return get_single_char_inner
 
@@ -157,23 +157,43 @@ def to_kpm(stats: dict):
     raw = get_stats([["kills", "all_time"], ["time", "all_time"]])(stats)
     kills, time_played = list(
         map(int, raw))
-    return kills / time_played
+    return kills / time_played * 60
 
 
 def kpm_from_char(client) -> Awaitable[ps2.Character]:
     return pipe_async(
         get_single_char(client),
         get_single_char_stats,
-        to_kpm
+        to_kpm,
+        lambda kpm: round(kpm, 3)
     )
+
+
+def kpm_generator(client):
+    def kpm_generator_inner(chars):
+        return map_async(kpm_from_char(client))(chars)
+    return kpm_generator_inner
+
+
+async def chunked_kpms(client, chars, chunk_size: int = 5, pause_seconds: float = 10.0):
+    def next_(gen): return gen.__anext__()
+    chunk = []
+    gen = kpm_generator(client)(chars)
+    for i in range(len(chars)):
+        # Force a delay to prevent rate limit
+        if i != 0 and i % chunk_size == 0:
+            yield chunk
+            chunk = []
+            await asyncio.sleep(pause_seconds)
+        chunk.append(await next_(gen))
 
 
 async def main():
     async with auraxium.Client(service_id=API_KEY) as client:
         chars = await get_chars(DTWM_ID)(client)
-        kpms = await flatten(map_async(kpm_from_char(client))(chars))
+        async for chunk in chunked_kpms(client, chars):
+            print([k for k in chunk])
 
-        print(kpms)
         # chars = await get_chars(DTWM_ID)(client)
         # res = await experimental(client)(chars[0])
         # pretty_print(res)
@@ -181,7 +201,7 @@ async def main():
         #    f.write(dumps(res))
         # https://census.daybreakgames.com/s:PS2Damagerequest/get/ps2:v2/single_character_by_id?character_id=5428926375525123617
         # That endpoint has probably everything I need
-        #outfit_kills = map_async(do_total_kills_query(client))(chars)
+        # outfit_kills = map_async(do_total_kills_query(client))(chars)
 
 
 asyncio.get_event_loop().run_until_complete(main())
