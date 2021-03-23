4f51474c85b86e0dc0e6c89a7de8f853570d4ff4
SMC242 <56912943+SMC242@users.noreply.github.com>
2021-03-23 22:44:58 +0000

    WIP // getting enemy kills - total kills

--- functions/teamkill_leaderboard.py
+++ functions/teamkill_leaderboard.py
@@ -5,6 +5,7 @@ from auraxium import ps2
 from auraxium.census import Query
 from json import dumps
 from inspect import iscoroutinefunction as is_coro_func, iscoroutine as is_coro
+from functools import reduce
 
 
 def read_file(path: str):
@@ -124,6 +125,8 @@ def get_single_char(client: auraxium.Client):
 
 
 def get_single_char_stats(data: dict):
+    """Get the stat history of a character.
+    This is useful for getting total stats like total kills"""
     keys = [
         "single_character_by_id_list",
         0,
@@ -139,46 +142,29 @@ def get_stats(keys: List[List[str]]):
     return get_stat_inner
 
 
-def to_kpm(stats: dict):
-    raw = get_stats([["kills", "all_time"], ["time", "all_time"]])(stats)
-    kills, time_played = list(
-        map(int, raw))
-    return kills / time_played * 60
+def get_stat_dict_from_char(char: dict):
+    return get_keys(["stats", "stat"])(char)
 
 
-def kpm_from_char(client) -> Awaitable[ps2.Character]:
-    return pipe_async(
-        get_single_char(client),
-        get_single_char_stats,
-        to_kpm,
-        lambda kpm: round(kpm, 3)
-    )
+def get_kill_records(char: dict):
+    stat_record = get_stat_dict_from_char(char)
+    def is_kills(record): return record["stat_name"] == "kills"
+    return list(filter(is_kills, stat_record))
 
 
-def kpm_generator(client):
-    def kpm_generator_inner(char_ids: List[int]):
-        return map_async(kpm_from_char(client))(char_ids)
-    return kpm_generator_inner
+def enemy_kills_from_record(record: dict) -> int:
+    return record["value_forever_vs"] + record["value_forever_tr"]
 
 
-async def chunked_kpms(client: auraxium.Client, char_ids: List[int], chunk_size: int = 5, pause_seconds: float = 10.0):
-    def next_(gen): return gen.__anext__()
-    chunk = []
-    gen = kpm_generator(client)(char_ids)
-    for i in range(len(char_ids)):
-        # Force a delay to prevent rate limit
-        if i != 0 and i % chunk_size == 0:
-            yield chunk
-            chunk = []
-            await asyncio.sleep(pause_seconds)
-        chunk.append(await next_(gen))
+def total_enemy_kills(kill_records: List[dict]) -> int:
+    def kills_from_record(acc, record): return acc + \
+        enemy_kills_from_record(record)
+    return list(reduce(kills_from_record, kill_records, 0))
 
 
 async def main():
     async with auraxium.Client(service_id=API_KEY) as client:
         ids = await get_outfit_char_ids(client)
-        async for chunk in chunked_kpms(client, ids):
-            print([k for k in chunk])
 
 
 asyncio.get_event_loop().run_until_complete(main())
