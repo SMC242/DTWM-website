5b90eae1e88992e21bee1ce480dfc9b3c135676e
SMC242 <56912943+SMC242@users.noreply.github.com>
2021-03-30 15:25:21 +0100

    OpenGraph

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,13 @@ import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import { AppProps } from "next/app";
 import Head from "next/head";
+import Skull from "../images/skull/DTWMSkull-16.png";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
       <Head>
+        {/* favicon stuff */}
         <link
           rel="apple-touch-icon"
           sizes="180x180"
@@ -29,6 +31,13 @@ function MyApp({ Component, pageProps }: AppProps) {
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="msapplication-TileColor" content="#9f00a7" />
         <meta name="theme-color" content="#e60ded" />
+
+        {/* OpenGraph stuff */}
+        <meta property="og:site_name" content="DTWM" />
+        <meta property="og:url" content="https://joindtwm.vercel.app/" />
+        <meta property="og:description" content="" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="images/skull/DTWMSkull.big.png" />
       </Head>
       <Component {...pageProps} />
     </ChakraProvider>
