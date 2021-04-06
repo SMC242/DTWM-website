4132860981f5930f95c9182314348e2cbab971b2
SMC242 <56912943+SMC242@users.noreply.github.com>
2021-04-06 21:26:39 +0100

    The button style works now

--- theme/components/button.ts
+++ theme/components/button.ts
@@ -2,11 +2,9 @@ import { ComponentStyleConfig } from "@chakra-ui/react";
 import { mode } from "@chakra-ui/theme-tools";
 
 const Button: ComponentStyleConfig = {
-  baseStyle: (props: object) => ({
-    bgGradient: mode(
-      "linear(to-b, pink.500, purple.500)",
-      "linear(to-b, green.200, red.300)"
-    )(props),
-  }),
+  baseStyle: {
+    background: "initial", // <---- this resets the background to whatever normal value it has
+    bgGradient: "linear(to-b, pink.500, purple.500)",
+  },
 };
 export default Button;
