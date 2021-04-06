34a34a594b50fde32a5b7b7e935885c7bff81916
SMC242 <56912943+SMC242@users.noreply.github.com>
2021-04-06 03:05:40 +0100

    A little bit of trolling UmU

--- components/theme_test.tsx
+++ components/theme_test.tsx
@@ -1,15 +1,31 @@
 import React from "react";
-import { Box, Button, useColorMode } from "@chakra-ui/react";
+import { Box, Button, useColorMode, Divider } from "@chakra-ui/react";
+
+const template = (
+  first: number,
+  second: number,
+  third: number,
+  fourth: number
+): string => `${first}.${second}.${third}.${fourth}`;
+const random = (column: number) => Math.floor(Math.random() * column);
+const ip_address = template(random(100), random(100), random(10), random(10));
 
 const ThemeTest = () => {
   const { colorMode, toggleColorMode } = useColorMode();
   return (
-    <Box>
+    <Box mx="5">
       <Button colorScheme="yellow">UmU</Button>
       <Button colorScheme="blue">AwA</Button>
       <Button colorScheme="pink" onClick={toggleColorMode}>
         Switch theme. Current mode: {colorMode}
       </Button>
+      <Divider my="5" />
+      Now I have your IP adress mwahahahahahahahahahaha
+      <br />
+      It is: {ip_address}
+      <br />
+      <Divider my="5" />
+      <small>I haven't actually grabbed your IP :)</small>
     </Box>
   );
 };
