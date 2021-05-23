904558ccafb5cbda4d5b736b5eca2d46e651fbd9
SMC242 <56912943+SMC242@users.noreply.github.com>
2021-05-23 04:44:52 +0100

    Tree component

---
+++ components/outfit_tree/tree.tsx
@@ -0,0 +1,29 @@
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
+import React, { FC, PropsWithChildren } from "react";
+import { Box, Circle, useColorModeValue } from "@chakra-ui/react";
+
+export interface TreeProps {}
+
+const Tree: FC<PropsWithChildren<TreeProps>> = ({ children }) => {
+  const color = useColorModeValue("black", "white");
+  return (
+    <TransformWrapper>
+      <TransformComponent>
+        <Box bg="transparent">
+          <Box float="right" borderColor={color} borderWidth="4px">
+            <u>Key</u>
+            <Circle borderColor={color} borderStyle="solid" borderWidth="1">
+              Alive outfit
+            </Circle>
+            <Circle borderColor={color} borderStyle="dashed" borderWidth="1">
+              Dead outfit
+            </Circle>
+          </Box>
+          {children}
+        </Box>
+      </TransformComponent>
+    </TransformWrapper>
+  );
+};
+
+export default Tree;
