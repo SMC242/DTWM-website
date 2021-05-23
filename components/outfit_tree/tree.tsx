92f660b71d296e7339ba103b1e9c8b2684007349
SMC242 <56912943+SMC242@users.noreply.github.com>
2021-05-23 04:57:25 +0100

    Trying to get the tree to fill the page

--- components/outfit_tree/tree.tsx
+++ components/outfit_tree/tree.tsx
@@ -9,24 +9,26 @@ export interface TreeProps {}
 const Tree: FC<PropsWithChildren<TreeProps>> = ({ children }) => {
   const color = useColorModeValue("black", "white");
   return (
-    <TransformWrapper>
-      <TransformComponent>
-        <Box bg="transparent">
-          <Box
-            float="right"
-            borderColor={color}
-            borderWidth="2px"
-            p={2}
-            marginLeft={2}
-          >
-            <u>Key</u>
-            <NodeCircle alive>Alive outfit</NodeCircle>
-            <NodeCircle alive={false}>Dead outfit</NodeCircle>
+    <Box height="100%" width="100%" marginTop={"1em"}>
+      <TransformWrapper>
+        <TransformComponent>
+          <Box bg="transparent" height="100%" width="100%">
+            <Box
+              float="right"
+              borderColor={color}
+              borderWidth="2px"
+              p={2}
+              marginLeft={2}
+            >
+              <u>Key</u>
+              <NodeCircle alive>Alive outfit</NodeCircle>
+              <NodeCircle alive={false}>Dead outfit</NodeCircle>
+            </Box>
+            {children}
           </Box>
-          {children}
-        </Box>
-      </TransformComponent>
-    </TransformWrapper>
+        </TransformComponent>
+      </TransformWrapper>
+    </Box>
   );
 };
 
