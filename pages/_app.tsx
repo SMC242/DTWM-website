09fa163fbef621781fc6f0505314b1154f9b7538
SMC242 <56912943+SMC242@users.noreply.github.com>
2021-05-28 10:11:55 +0100

    Using new header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import type { AppProps } from "next/app";
 import Head from "next/head";
 import { CustomChakraProvider } from "../theme";
 import { HNavbar } from "../components/navigation/navbar";
-import OG from "../components/templates/open_graph";
+import Header from "../components/templates/header";
 
 const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
   const links = [
@@ -29,32 +29,7 @@ const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
 
   return (
     <CustomChakraProvider>
-      <Head>
-        {/* favicon stuff */}
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-        <meta name="msapplication-TileColor" content="#9f00a7" />
-        <meta name="theme-color" content="#e60ded" />
-
-        <OG />
-      </Head>
+      <Header />
       <HNavbar links={links} />
       <Component {...pageProps} />
     </CustomChakraProvider>
--- pages/outfit_trees/outfit_trees_home.tsx
+++ pages/outfit_trees/outfit_trees_home.tsx
@@ -4,19 +4,17 @@ import React from "react";
 import { UnorderedList, ListItem, Heading } from "@chakra-ui/react";
 
 import Article from "../../components/templates/article";
-import OG from "../../components/templates/open_graph";
+import Header from "../../components/templates/header";
 
 const OutfitTreesHomePage = () => {
   const trees = [{ name: "MB1", route: "/outfit_trees/MB1" }];
   return (
     <>
-      <Head>
-        <title>Outfit Tree Home Page</title>
-        <OG
-          site_name="DTWM - Outfit Trees"
-          description="List of the compiled outfit trees."
-        />
-      </Head>
+      <Header
+        site_name="DTWM - Outfit Trees"
+        description="List of the compiled outfit trees."
+        title="Outfit Tree Home Page"
+      />
       <Article>
         <Heading>Available outfit family trees</Heading>
         <UnorderedList>
