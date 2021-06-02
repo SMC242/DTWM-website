07b3ceb38998b5bec108dc21e5da443e7ff64819
SMC242 <56912943+SMC242@users.noreply.github.com>
2021-06-02 14:38:26 +0100

    Added doc extension

--- api/index.py
+++ api/index.py
@@ -1,9 +1,14 @@
 from sanic import Sanic
-from sanic.response import json, HTTPResponse
 from sanic.request import Request
-app = Sanic(name="DTWM API root")
+from sanic.response import HTTPResponse, JSON
+
+from sanic_openapi import openapi3_blueprint
 
 
-@app.route('/')
-async def index(request: Request):
-    return json({'hello': "yes"})
+app = Sanic("DTWM API index endpoint")
+app.blueprint(openapi3_blueprint)
+
+
+@app.route("/")
+async def index(request: Request) -> HTTPResponse:
+    return JSON({"message": "This is the DTWM API. Please visit /api/swagger if you are a developer. Otherwise, it's best to leave."})
