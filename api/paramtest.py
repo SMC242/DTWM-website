fc6805f5ecad3eb02edfe14e8c92d6d6978c1680
SMC242 <56912943+SMC242@users.noreply.github.com>
2021-06-02 11:18:58 +0100

    Fixed paramtest path

--- api/paramtest.py
+++ api/paramtest.py
@@ -4,6 +4,6 @@ from sanic.request import Request
 app = Sanic(name="DTWM API parameters test")
 
 
-@app.route("/paramtest/<param>")
+@app.route("/api/paramtest/<param>")
 async def pathtest(request: Request, param: str) -> HTTPResponse:
     return json({"Parameter works": param})
