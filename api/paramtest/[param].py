8dd7befef5a52373590ac5443a2f708f8cc8c023
SMC242 <56912943+SMC242@users.noreply.github.com>
2021-06-02 14:38:14 +0100

    Done testing

--- api/paramtest/[param].py
+++
@@ -1,9 +0,0 @@
-from sanic import Sanic
-from sanic.response import json, HTTPResponse
-from sanic.request import Request
-app = Sanic(name="DTWM API parameters test")
-
-
-@app.route("/api/paramtest/<param:string>")
-async def pathtest(request: Request, param="failed") -> HTTPResponse:
-    return json({"Parameter works": param})
--- api/pathtest.py
+++
@@ -1,9 +0,0 @@
-from sanic import Sanic
-from sanic.response import json, HTTPResponse
-from sanic.request import Request
-app = Sanic(name="DTWM API path testing")
-
-
-@app.route("/api/pathtest")  # must prefix with /api/
-async def pathtest(request: Request) -> HTTPResponse:
-    return json({"non-index path works": True})
