bc607f2111679c7b0467f71be88a00eb88da97c5
SMC242 <56912943+SMC242@users.noreply.github.com>
2021-06-02 14:11:20 +0100

    The colon in the angular brackets is a type

--- api/paramtest/[param].py
+++ api/paramtest/[param].py
@@ -4,6 +4,6 @@ from sanic.request import Request
 app = Sanic(name="DTWM API parameters test")
 
 
-@app.route("/api/paramtest/<test:test>")
+@app.route("/api/paramtest/<test:string>")
 async def pathtest(request: Request, param="failed") -> HTTPResponse:
     return json({"Parameter works": param})
