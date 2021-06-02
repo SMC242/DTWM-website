625b82f034501d04f8ff2e937068857e11952968
SMC242 <56912943+SMC242@users.noreply.github.com>
2021-06-02 12:29:18 +0100

    Unique paths

--- api/[paramtest].py
+++ api/[paramtest].py
@@ -4,6 +4,6 @@ from sanic.request import Request
 app = Sanic(name="DTWM API parameters test")
 
 
-@app.route("/api/paramtest/<param:param>")
+@app.route("/api/paramtest/<test:test>")
 async def pathtest(request: Request, param: str = "failed") -> HTTPResponse:
     return json({"Parameter works": param})
