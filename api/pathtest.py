e9bfbeb1ce52911412ffb60dd648d4abe98a42b7
SMC242 <56912943+SMC242@users.noreply.github.com>
2021-06-02 11:18:36 +0100

    I have to add the /api/ prefix

--- api/pathtest.py
+++ api/pathtest.py
@@ -4,6 +4,6 @@ from sanic.request import Request
 app = Sanic(name="DTWM API path testing")
 
 
-@app.route("/api/pathtest")
+@app.route("/api/pathtest")  # must prefix with /api/
 async def pathtest(request: Request) -> HTTPResponse:
     return json({"non-index path works": True})
