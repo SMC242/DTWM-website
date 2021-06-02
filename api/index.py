d8b998eaf064a3bcc62c0aebccea78b0d7b8a0d0
SMC242 <56912943+SMC242@users.noreply.github.com>
2021-06-02 11:02:44 +0100

    Removed duplicate route

--- api/index.py
+++ api/index.py
@@ -8,8 +8,3 @@ app = Sanic(name="DTWM API root")
 @app.route('/<path:path>')
 async def index(request: Request, path=""):
     return json({'hello': path})
-
-
-@app.route("/pathtest")
-async def pathtest(request) -> HTTPResponse:
-    return json({"non-index path works"})
