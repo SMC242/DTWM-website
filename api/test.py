b0b120ac5a335a135cdaab2a56862be1e39d9f55
SMC242 <56912943+SMC242@users.noreply.github.com>
2021-06-02 09:36:48 +0100

    Seeing if the app is actually running

--- api/test.py
+++ api/test.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
+from uvicorn import run
 
 app = FastAPI()
+print("App running")
 
 
 @app.get("/test")
 async def test_standalone_app() -> dict:
     return {"message": "Standalone app works"}
+
+if __name__ == "__main__":
+    run(app)
