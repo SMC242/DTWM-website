78177cffc12f4679bcaa05fab665606cea2d6d7a
SMC242 <56912943+SMC242@users.noreply.github.com>
2021-06-02 11:13:01 +0100

    Path not working

--- api/pathtest.py
+++ api/pathtest.py
@@ -4,6 +4,6 @@ from sanic.request import Request
 app = Sanic(name="DTWM API path testing")
 
 
-@app.route("/pathtest")
+@app.route("/api/pathtest")
 async def pathtest(request: Request) -> HTTPResponse:
     return json({"non-index path works": True})
